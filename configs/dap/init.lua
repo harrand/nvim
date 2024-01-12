5d661e21456d8e468b132814728c61c3d22b4f86
harrand <harrysjh98@gmail.com>
2024-01-12 01:08:29 +0000

    Much richer DAP interaction. Manual launch now dynamically appends the ran program as a new configuration, meaning you can run it again even after running something else earlier. Run last config now indicates the name of the last config

--- configs/dap/init.lua
+++ configs/dap/init.lua
@@ -6,7 +6,7 @@ require("custom.configs.dap.ui")
 dap.adapters.cppdbg = {
   id = 'cppdbg',
   type = 'executable',
-  command = 'C:\\Users\\Harry\\.vscode\\extensions\\ms-vscode.cpptools-1.18.5-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe',
+  command = '$USERPROFILE\\.vscode\\extensions\\ms-vscode.cpptools-1.18.5-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe',
   options = {
     detached = false
   }
@@ -25,19 +25,12 @@ dap.cache.last_cwd = nil
 
 dap.configurations.cpp = {
   {
+	-- note that program and cwd are empty here. this is because this configuration is blessed and has a __call metatable entry. see below.
     name = "Manual Launch",
     type = "lldb",
     request = "launch",
-    program = function()
-		local prog = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		dap.cache.last_program = prog
-		return prog
-    end,
-    cwd = function()
-		local cwd = vim.fn.input('Working Directory: ', vim.fn.getcwd() .. '/', 'file')
-		dap.cache.last_cwd = cwd
-		return cwd
-    end,
+    program = dap.ABORT,
+    cwd = dap.ABORT,
     stopAtEntry = true,
   },
   {
@@ -45,11 +38,44 @@ dap.configurations.cpp = {
     type = "lldb",
     request = "launch",
     program = function()
-		return dap.cache.last_program
+		return dap.cache.last_program or dap.ABORT
     end,
     cwd = function()
-		return dap.cache.last_cwd
+		return dap.cache.last_cwd or dap.ABORT
     end,
     stopAtEntry = true,
   },
 }
+
+function on_manual_launch(par)
+	local cur_cwd = vim.fn.getcwd()
+	local prog = vim.fn.input('Path to executable: ', cur_cwd .. '/', 'file')
+	local cwd = vim.fn.input('Working Directory: ', cur_cwd .. '/', 'file')
+	par.program = prog
+	par.cwd = cwd
+	if prog ~= cur_cwd then
+		-- if prog == cur_cwd then they skipped past the program bit, which is 100% a mistake or they want to early out.
+		-- if not, we assume its a valid exe location.
+		local new_config = {}
+		new_config.name = prog
+		new_config.type = "lldb"
+		new_config.request = "launch"
+		new_config.program = prog
+		new_config.cwd = cwd
+		dap.cache.last_program = prog
+		dap.cache.last_cwd = cwd
+		table.insert(dap.configurations.cpp, new_config)
+
+		-- set last-program's config name to indicate what the last prog was.
+		dap.configurations.cpp[2].name = "Last Program (" .. dap.cache.last_program .. ")"
+	else
+		-- user hasn't entered enough valid data. set the config to be abort (meaning nothing runs.)
+		par.program = dap.ABORT
+		par.cwd = dap.ABORT
+	end
+	return par
+end
+
+setmetatable(dap.configurations.cpp[1], {__call = on_manual_launch})
+-- dap.configurations.NvimTree normally means the user accidentally invoked dap-continue when nvimtree was focused. As I'm a C++ developer, I'm gonna assume they're using c++.
+dap.configurations.NvimTree = dap.configurations.cpp
